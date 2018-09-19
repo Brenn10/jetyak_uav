f9481f24022a8940bf2550b5cda5c9616d3b85b7
Brennan Cain <brenn.cain@gmail.com>
2018-09-19 15:34:18 -0400

    Moving to new architecture

--- jetyak_uav_utils/include/jetyak_uav_utils/behaviors.h
+++ jetyak_uav_utils/include/jetyak_uav_utils/behaviors.h
@@ -27,63 +27,130 @@
 #include "../lib/bsc_common/include/util.h"
 
 class behaviors {
-  private:
-    bsc_common::PID *xpid_,*ypid_,*zpid_,*wpid_;
-    geometry_msgs::Quaternion uavPose_; // holds xyz position and w yaw
-
-    ros::Subscriber arTagSub_;
-    ros::Publisher cmdPub_;
-    ros::ServiceClient taskSrv_;
-    ros::ServiceServer modeService_;
-    bool behaviorChanged=false;
-    char currentMode_;
-    double tagLastSeen_;
+private:
 
 
-    // Land specific constants
-    struct {
-      geometry_msgs::Quaternion kp,kd,ki;
-      const double START_HEIGHT=3.0;
-      double currGoalHeight;
-      double collapseRatio;
+  /*********************************************
+  * ROS PUBLISHERS, SUBSCRIBERS, AND SERVICES
+  *********************************************/
+  ros::Subscriber tagPoseSub_, boatGPSSub_, boatAttSub_, uavGPSSub_, uavAttSub_;
+  ros::Publisher cmdPub_;
+  ros::ServiceClient taskSrv_;
+  ros::ServiceServer modeService_;
 
-    } land_;
+  /**********************
+  * INSTANCE VARIABLES
+  **********************/
+  bsc_common::PID *xpid_,*ypid_,*zpid_,*wpid_; // pid controllers
+  bool behaviorChanged=false;
+  bool tagDetected=false;
+  char currentMode_=0;
+  double tagLastSeen_=0;
 
-    // Land specific constants
-    struct {
-      geometry_msgs::Quaternion kp,kd,ki;
-      geometry_msgs::Quaternion follow_pose;
-    } follow_;
+  /************************************
+  * STATE VARIABLES
+  ************************************/
+  sensor_msgs::NavSatFix uavGPS_,boatGPS_;
+  sensor_msgs::QuaternionStamped uavAttitude_;
+  std_msgs::Float64 boatHeading_;
+  sensor_msgs::PoseStamped tagPose_;
 
-    /**
-    * Changes the current mode
-    * changes control_priority
-    *
-    * @param msg gets the mode from the broadcast
-    */
-    bool modeCallback(jetyak_uav_utils::Mode::Request  &req,
-                      jetyak_uav_utils::Mode::Response &res);
-    /**
-    * Changes the current mode
-    * changes control_priority
-    *
-    * @param msg gets the mode from the broadcast
-    */
-    void arTagCallback(const ar_track_alvar_msgs::AlvarMarkers::ConstPtr& msg);
 
-  public:
-    /** Constructor
-    * Start up the Controller Node
-    * Create publishers, subscribers, and request access
-    *
-    * @param nh node handler
-    */
-    behaviors(ros::NodeHandle& nh);
+  /*********************************************
+  * BEHAVIOR SPECIFIC VARIABLES AND CONSTANTS
+  **********************************************/
+  // Land specific constants
+  struct {
+    geometry_msgs::Quaternion kp,kd,ki;
+    const double START_HEIGHT=3.0;
+    double currGoalHeight;
+    double collapseRatio;
 
-    ~behaviors();
-    /** publishCommand
-    * Calls the cmdPub on the highest priority command passed in
-    */
+  } land_;
+
+  // follow specific constants
+  struct {
+    geometry_msgs::Quaternion kp,kd,ki;
+    geometry_msgs::Quaternion follow_pose;
+  } follow_;
+
+  /*********************
+  * SERVICE CALLBACKS
+  *********************/
+  /** modeCallback
+  * Callback for the mode service. Changes the current mode.
+  *
+  * @param msg gets the mode from the broadcast
+  */
+  bool modeCallback(jetyak_uav_utils::Mode::Request  &req,
+                    jetyak_uav_utils::Mode::Response &res);
+
+
+  /****************************
+  * SUBSCRIPTION CALLBACKS
+  *****************************/
+
+
+  /** tagPoseCallback
+  * Changes the current mode
+  * changes control_priority
+  *
+  * @param msg gets the pose of the tag relative to the UAV
+  */
+  void tagPoseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg);
+
+  /** uavGPSCallback
+  * Listens for updates from the UAVs GPS
+  *
+  * @param msg gets the global position of the UAV
+  */
+  void uavGPSCallback(const sensor_msgs::NavSatFix::ConstPtr& msg);
+
+  /** boatGPSCallback
+  * Listens for updates from the boat's GPS
+  *
+  * @param msg gets the global position of the boat
+  */
+  void boatGPSCallback(const sensor_msgs::NavSatFix::ConstPtr& msg);
+
+  /** uavAttitudeCallback
+  * Listens for updates in the UAVs Attitude
+  *
+  * @param msg gets the global attitude of the UAV
+  */
+  void uavAttitudeCallback(const sensor_msgs::QuaternionStamped::ConstPtr& msg);
+
+  /** boatIMUCallback
+  * Listens for updates in the boat's orientation
+  *
+  * @param msg gets the heading of the boat
+  */
+  void boatIMUCallback(const sensor_msgs::Imu::ConstPtr& msg);
+
+
+  /******************************
+  * BEHAVIOR METHODS
+  ******************************/
+  /** takeoffBehavior
+   * implements the takeoff behavior
+  */
+  void takeoffBehavior();
+public:
+  /** Constructor
+  * Start up the Controller Node
+  * Create publishers, subscribers, services
+  *
+  * @param nh node handler
+  */
+  behaviors(ros::NodeHandle& nh);
+
+
+  ~behaviors();
+
+  /** doBehaviorAction
+   * calls the behavior designated by the mode service
+  */
+  void doBehaviorAction();
 };
 
 #endif
--- jetyak_uav_utils/src/behaviors.cpp
+++ jetyak_uav_utils/src/behaviors.cpp
@@ -6,177 +6,25 @@ behaviors::behaviors(ros::NodeHandle& nh):
   zpid_(NULL),
   wpid_(NULL)
  {
-  arTagSub_ = nh.subscribe("/ar_pose_marker",1,&behaviors::arTagCallback, this);
-  cmdPub_ = nh.advertise<sensor_msgs::Joy>("jetyak_uav_utils/behaviorCmd",1);
-  modeService_ = nh.advertiseService("jetyak_uav_utils/mode", &behaviors::modeCallback,this);
+  //subscribers
+  tagPoseSub_ = nh.subscribe("/jetyak_uav_utils/tag_pose",1,&behaviors::tagPoseCallback, this);
+  uavGPSSub_ = nh.subscribe("/dji_sdk/gps_position",1,&behaviors::uavGPSCallback, this);
+  boatGPSSub_ = nh.subscribe("boat_gps",1,&behaviors::uavGPSCallback, this);
+  uavAttSub_ =  nh.subscribe("/dji_sdk/attitude",1,&behaviors::uavAttitudeCallback, this);
+  boatIMUSub_ =  nh.subscribe("boat_imu",1,&behaviors::boatHeadingCallback, this);
+
+  //Publishers
+  cmdPub_ = nh.advertise<sensor_msgs::Joy>("jetyak_uav_utils/behavior_cmd",1);
+
+  //service clients
   taskSrv_ = nh.serviceClient<dji_sdk::DroneTaskControl>("/dji_sdk/drone_task_control");
 
-
-  currentMode_ = 0;
-
+  // Service servers
+  modeService_ = nh.advertiseService("jetyak_uav_utils/mode", &behaviors::modeCallback,this);
 }
 
 behaviors::~behaviors() {}
 
-void behaviors::arTagCallback(const ar_track_alvar_msgs::AlvarMarkers::ConstPtr& msg) {
-  // If tags detected
-  if(!msg->markers.empty()){
-    //get time of detection
-    tagLastSeen_=ros::Time::now().toSec();
-
-    // get the transform from the tag to the uav
-    geometry_msgs::Pose pose_from_tag;
-    bsc_common::util::inverse_pose(msg->markers[0].pose.pose,pose_from_tag);
-
-    //convert the orientation to rpy
-    const geometry_msgs::Quaternion* orientation = const_cast<const geometry_msgs::Quaternion*>(&pose_from_tag.orientation);
-    geometry_msgs::Vector3* state = new geometry_msgs::Vector3();
-    bsc_common::util::rpy_from_quat(orientation,state);//convert quat to rpy
-
-    //save pose
-    uavPose_.x=pose_from_tag.position.x;
-    uavPose_.y=pose_from_tag.position.y;
-    uavPose_.z=pose_from_tag.position.z;
-    uavPose_.w=state->z;
-
-    delete state;
-
-
-    /***************************************************************************
-    * Behavior actions based on ar_tag
-    ***************************************************************************/
-    switch(this->currentMode_)
-    {
-      // LAND BEHAVIOR HERE
-      case jetyak_uav_utils::Mode::Request::LAND:
-      {
-        /* If close enough, request a landing from sdk
-         * If first time, reset controller and do set constants
-         * else decrease distance and update controller
-         */
-        bool closeEnough = uavPose_.z<.1;
-
-
-        if(closeEnough)
-        {
-          dji_sdk::DroneTaskControl srv;
-          srv.request.task=6;
-          taskSrv_.call(srv);
-          if(srv.response.result)
-          {
-            this->currentMode_=jetyak_uav_utils::Mode::Request::LANDED;
-          }
-          else
-          {
-            ROS_WARN("Failed to land, hovering");
-            this->currentMode_=jetyak_uav_utils::Mode::Request::HOVER;
-          }
-        }
-        else if(behaviorChanged)
-        {
-          land_.currGoalHeight=land_.START_HEIGHT;
-
-          behaviorChanged=false;
-
-          // update PID Constants
-          xpid_->updateParams(land_.kp.x,land_.ki.x,land_.kd.x);
-          ypid_->updateParams(land_.kp.y,land_.ki.y,land_.kd.y);
-          zpid_->updateParams(land_.kp.z,land_.ki.z,land_.kd.z);
-          wpid_->updateParams(land_.kp.w,land_.ki.w,land_.kd.w);
-
-          // reset PIDs
-          xpid_->reset();
-          ypid_->reset();
-          zpid_->reset();
-          wpid_->reset();
-
-        }
-        else
-        {
-          //collapse height
-          land_.currGoalHeight*=land_.collapseRatio;
-
-          //update controllers
-          xpid_->update(-uavPose_.x);
-          ypid_->update(-uavPose_.y);
-          zpid_->update(land_.currGoalHeight-uavPose_.z);
-          wpid_->update(-uavPose_.w);
-
-          //rotate velocities in reference to the uav
-          double rotated_x;
-          double rotated_y;
-          bsc_common::util::rotate_vector(
-            xpid_->get_signal(),ypid_->get_signal(),-uavPose_.w,rotated_x,rotated_y);
-
-          // set cmd things and publish
-          sensor_msgs::Joy cmd;
-          cmd.axes.push_back(rotated_x);
-          cmd.axes.push_back(rotated_y);
-          cmd.axes.push_back(wpid_->get_signal());
-          cmd.axes.push_back(zpid_->get_signal());
-          cmdPub_.publish(cmd);
-        }
-
-        break;
-      }
-      // FOLLOW BEHAVIOR HERE
-      case jetyak_uav_utils::Mode::Request::FOLLOW: {
-        if(behaviorChanged)
-        {
-          behaviorChanged=false;
-
-          // update PID Constants
-          xpid_->updateParams(follow_.kp.x,follow_.ki.x,follow_.kd.x);
-          ypid_->updateParams(follow_.kp.y,follow_.ki.y,follow_.kd.y);
-          zpid_->updateParams(follow_.kp.z,follow_.ki.z,follow_.kd.z);
-          wpid_->updateParams(follow_.kp.w,follow_.ki.w,follow_.kd.w);
-
-          // reset PIDs
-          xpid_->reset();
-          ypid_->reset();
-          zpid_->reset();
-          wpid_->reset();
-        }
-        else
-        {
-          //update controllers
-          xpid_->update(follow_.follow_pose.x-uavPose_.x);
-          ypid_->update(follow_.follow_pose.y-uavPose_.y);
-          zpid_->update(follow_.follow_pose.z-uavPose_.z);
-          wpid_->update(bsc_common::util::ang_dist(follow_.follow_pose.w,uavPose_.w));//TODO: Check sign
-
-          //rotate velocities in reference to the uav
-          double rotated_x;
-          double rotated_y;
-          bsc_common::util::rotate_vector(
-            xpid_->get_signal(),ypid_->get_signal(),-uavPose_.w,rotated_x,rotated_y);
-
-          // set cmd things and publish
-          sensor_msgs::Joy cmd;
-          cmd.axes.push_back(rotated_x);
-          cmd.axes.push_back(rotated_y);
-          cmd.axes.push_back(wpid_->get_signal());
-          cmd.axes.push_back(zpid_->get_signal());
-          cmdPub_.publish(cmd);
-        }
-        break;
-      }
-      // RETURN BEHVAIOR HERE
-      case jetyak_uav_utils::Mode::Request::RETURN: {
-
-        break;
-      }
-      case jetyak_uav_utils::Mode::Request::HOVER: {
-
-        break;
-      }
-    }
-
-
-
-
-  }
-}
 
 bool behaviors::modeCallback(jetyak_uav_utils::Mode::Request  &req,
                               jetyak_uav_utils::Mode::Response &res) {
@@ -212,20 +60,23 @@ bool behaviors::modeCallback(jetyak_uav_utils::Mode::Request  &req,
   return res.success;
 }
 
+void behaviors::tagPoseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
+  
+}
+
 int main(int argc, char **argv) {
   ros::init(argc,argv,"uav_behaviors");
   ros::NodeHandle nh;
   behaviors uav_behaviors(nh);
-  ros::spin();
-  // ros::Rate rate(10);
-  //
-  // while(ros::ok())
-  // {
-  //   ros::spinOnce();
-  //
-  //   uav_behaviors.publishCommand();
-  //
-  //   rate.sleep();
-  // }
+  ros::Rate rate(10);
+
+  while(ros::ok())
+  {
+    ros::spinOnce();
+
+    uav_behaviors.doBehaviorAction();
+
+    rate.sleep();
+  }
   return 0;
 }
