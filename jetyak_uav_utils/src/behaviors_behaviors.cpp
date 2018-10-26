dec0b55c4f015422873812caac3abdfd003c5011
Brennan Cain <brenn.cain@gmail.com>
2018-10-26 09:55:11 -0400

    adding return

--- jetyak_uav_utils/src/behaviors_behaviors.cpp
+++ jetyak_uav_utils/src/behaviors_behaviors.cpp
@@ -21,10 +21,7 @@ void behaviors::takeoffBehavior() {
 }
 
 void behaviors::followBehavior() {
-  /*if(!propellorsRunning) {
-    ROS_ERROR("%s","Propellors not running, unable to follow");
-  }
-  else */
+
   if(behaviorChanged_) { // if just changed
     follow_.lastSpotted=actualPose_.t;
     follow_.lostTagCounter=0;
@@ -103,7 +100,79 @@ void behaviors::followBehavior() {
 void behaviors::returnBehavior() {};
 
 void behaviors::landBehavior() {
+  if(behaviorChanged_) { // if just changed
+    land_.lastSpotted=actualPose_.t;
+    land_.lostTagCounter=0;
+    xpid_->reset();
+    ypid_->reset();
+    zpid_->reset();
+    wpid_->reset();
+    xpid_->updateParams(land_.kp.x ,land_.ki.x,land_.kd.x);
+    ypid_->updateParams(land_.kp.y ,land_.ki.y,land_.kd.y);
+    zpid_->updateParams(land_.kp.z ,land_.ki.z,land_.kd.z);
+    wpid_->updateParams(land_.kp.w ,land_.ki.w,land_.kd.w);
+    behaviorChanged_=false;
 
+  } else { // DO the loop
+
+    if(follow_.lastSpotted!=actualPose_.t) { //if time changed
+      follow_.lastSpotted=actualPose_.t;
+      follow_.lostTagCounter=0;
+    }
+    else { //if time is same
+      follow_.lostTagCounter++;
+
+      if(follow_.lostTagCounter>3 and follow_.lostTagCounter<=20) { //if time has been same for over 3 tick
+  ROS_WARN("No tag update: %i" , follow_.lostTagCounter);
+        sensor_msgs::Joy cmd;
+        cmd.axes.push_back(0);
+        cmd.axes.push_back(0);
+        cmd.axes.push_back(0);
+        cmd.axes.push_back(actualPose_.y>0 ? .1 : -.1); //rotate CCW if lost on left, CW if right
+        cmd.axes.push_back(bodyVelCmdFlag_);
+        cmdPub_.publish(cmd);
+        return;
+      } else if (follow_.lostTagCounter>20){ // if time has been same for over 10 tick
+        ROS_WARN("Tag Lost");
+  sensor_msgs::Joy cmd;
+  cmd.axes.push_back(0);
+  cmd.axes.push_back(0);
+  cmd.axes.push_back(0);
+  cmd.axes.push_back(0);
+  cmd.axes.push_back(bodyVelCmdFlag_);
+  cmdPub_.publish(cmd);
+        return;
+      }
+    }
+
+    // line up with pad
+    xpid_->update(follow_.follow_pose.x-actualPose_.x,actualPose_.t);
+    ypid_->update(follow_.follow_pose.y-actualPose_.y,actualPose_.t);
+    zpid_->update(follow_.follow_pose.z-actualPose_.z,actualPose_.t);
+    wpid_->update(follow_.follow_pose.w-actualPose_.w,actualPose_.t);
+
+  /*    //rotate velocities in reference to the tag
+    double rotated_x;
+    double rotated_y;
+    bsc_common::util::rotate_vector(
+      xpid_->get_signal(),ypid_->get_signal(),-actualPose_.w,rotated_x,rotated_y);
+  */
+    ROS_INFO("sig x: %1.2f, y:%1.2f, z: %1.2f, yaw: %1.3f",
+      -xpid_->get_signal(),
+      -ypid_->get_signal(),
+      -zpid_->get_signal(),
+      -wpid_->get_signal()
+    );
+    sensor_msgs::Joy cmd;
+    cmd.axes.push_back(-xpid_->get_signal());
+    cmd.axes.push_back(-ypid_->get_signal());
+    cmd.axes.push_back(-zpid_->get_signal());
+    cmd.axes.push_back(-wpid_->get_signal());
+    cmd.axes.push_back(bodyVelCmdFlag_);
+    cmdPub_.publish(cmd);
+
+
+  }
 };
 
 void behaviors::rideBehavior() {
