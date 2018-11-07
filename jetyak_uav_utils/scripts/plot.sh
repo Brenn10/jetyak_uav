2a4279ee43cb9f6e07b5af4b553d93a1355073e0
Brennan Cain <brenn.cain@gmail.com>
2018-11-07 09:55:22 -0500

    fixing plotter

--- jetyak_uav_utils/scripts/plot.sh
+++ jetyak_uav_utils/scripts/plot.sh
@@ -2,5 +2,5 @@
 
 ssh ubuntu@$1 "./git/jetyak_uav/jetyak_uav_utils/scripts/updateLatest.sh"
 scp ubuntu@$1:~/git/jetyak_uav/jetyak_uav_utils/scripts/latest.ignore .
-scp ubuntu@$1:/media/ubuntu/Manfold/FlightLog/`cat latest.ignore` .
+scp ubuntu@$1:/media/ubuntu/Manifold/FlightLog/`cat latest.ignore` .
 python plotter.py $(ls -rt . | tail -n1)
