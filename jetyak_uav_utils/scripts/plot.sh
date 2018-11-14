ee839a5a8a85b73f6a44c55e3100595f5bd21c47
Brennan Cain <brenn.cain@gmail.com>
2018-11-14 13:57:28 -0500

    fixed n3 cconflict

--- jetyak_uav_utils/scripts/plot.sh
+++ jetyak_uav_utils/scripts/plot.sh
@@ -1,5 +1,6 @@
 # Run on client machine
 
-ssh ubuntu@10.34.7.41 "./git/jetyak_uav/jetyak_uav_utils/scripts/updateLatest.sh"
-scp -r ubuntu@10.34.7.41:~/git/jetyak_uav/jetyak_uav_utils/scripts/latest .
+ssh ubuntu@$1 "./git/jetyak_uav/jetyak_uav_utils/scripts/updateLatest.sh"
+scp ubuntu@$1:~/git/jetyak_uav/jetyak_uav_utils/scripts/latest.ignore .
+scp ubuntu@$1:/media/ubuntu/Manifold/FlightLog/`cat latest.ignore` .
 python plotter.py $(ls -rt . | tail -n1)
--- jetyak_uav_utils/scripts/updateLatest.sh
+++ jetyak_uav_utils/scripts/updateLatest.sh
@@ -1,5 +1,5 @@
 # Place in the flight log directory of the robot
 
 
-rm latest
-ln -s /media/ubuntu/Manifold/FlightLog/`ls -rt /media/ubuntu/Manifold/FlightLog | tail -n1` latest
+touch latest.ignore
+ls -rt /media/ubuntu/Manifold/FlightLog | tail -n1 > latest.ignore
