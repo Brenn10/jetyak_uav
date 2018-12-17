2e6d33d9dab95d49ba2a0f1617c50e86751d56de
Brennan Cain <brenn.cain@gmail.com>
2018-12-17 11:30:52 -0500

    fixing turning

--- jetyak_uav_utils/src/behaviors_behaviors.cpp
+++ jetyak_uav_utils/src/behaviors_behaviors.cpp
@@ -82,17 +82,19 @@ void behaviors::followBehavior() {
       follow_.lostTagCounter++;
 
       if(follow_.lostTagCounter>3 and follow_.lostTagCounter<=20) { //if time has been same for over 3 tick
-        ROS_WARN("No tag update: %i" , follow_.lostTagCounter);
+        //ROS_WARN("No tag update: %i" , follow_.lostTagCounter);
         sensor_msgs::Joy cmd;
         cmd.axes.push_back(0);
         cmd.axes.push_back(0);
         cmd.axes.push_back(0);
-        cmd.axes.push_back(simpleTag_.y>0 ? 1 : -1); //rotate CCW if lost on left, CW if right
+        //cmd.axes.push_back(simpleTag_.y>0 ? 1 : -1); //rotate CCW if lost on left, CW if right
+        cmd.axes.push_back(0);
+
         cmd.axes.push_back(bodyVelCmdFlag_);
         cmdPub_.publish(cmd);
         return;
       } else if (follow_.lostTagCounter>20){ // if time has been same for over 10 tick
-        ROS_WARN("Tag Lost");
+        //ROS_WARN("Tag Lost");
       	sensor_msgs::Joy cmd;
       	cmd.axes.push_back(0);
       	cmd.axes.push_back(0);
